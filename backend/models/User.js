c8a74c9b36496b81a63c2328769dd48903c3d48b
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2023-07-28 16:59:20 +0100

    adding timetamps to User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,38 +1,41 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: { type: String },
+    address: { type: String },
+    roles: {
+      type: [String],
+      default: ["customer"],
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    refreshToken: String,
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  phoneNumber: { type: String },
-  address: { type: String },
-  roles: {
-    type: [String],
-    default: ["customer"],
-  },
-  active: {
-    type: Boolean,
-    default: true,
-  },
-  refreshToken: String,
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
