ac1751b783a87b005f2502a471bdfe01a6e5ac7c
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2023-08-02 16:18:33 +0100

    defining routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./features/User/Login";
+import SignUp from "./features/USer/SignUp";
+import SharedLayout from "./components/SharedLayout";
+import Home from "./pages/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
