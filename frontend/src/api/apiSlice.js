495ef9260955947d02768a2cd38c273143940dcd
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2023-08-02 15:35:41 +0100

    setup Readux Toolkit + RTKQ

---
+++ frontend/src/api/apiSlice.js
@@ -0,0 +1,9 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://127.0.0.1:3500" }),
+  endpoints: (builder) => ({}),
+});
+
+export const {} = apiSlice;
