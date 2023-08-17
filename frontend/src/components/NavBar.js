dbe91102e9e9c38f1ff710c6c20ff35390e42733
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2023-08-17 17:04:57 +0100

    working on responsive navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,47 +1,54 @@
-import { FaSearch, FaShoppingCart, FaHeart } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
+import { AiOutlineShoppingCart, AiOutlineMenu } from "react-icons/ai";
+import { MdFavoriteBorder } from "react-icons/md";
+import { GoPerson } from "react-icons/go";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="bg-stone-200 text-black w-full">
-      <div className="container mx-auto flex justify-between items-center py-3">
-        <div className="text-xl font-bold">LOGO</div>
-        <div className="flex items-center space-x-6">
-          <div className="relative">
+      <div className="container mx-auto flex justify-between items-center py-3 max-w-[1200px] px-6">
+        <div className="text-xl font-bold mr-2">LOGO</div>
+        <div className="flex items-center sm:space-x-6 ss:space-x-4 space-x-3">
+          <div className="relative ss:block hidden">
             <input
               type="text"
               placeholder="Search products..."
-              className="py-3 px-4 pr-10 text-gray-700 w-64 sm:w-96 focus:outline-none bg-white border border-gray-300 rounded-full transition-all duration-300 focus:ring-2 focus:ring-indigo-300"
+              className="py-3 px-4 pr-10 text-gray-700 w-64 sm:w-80 focus:outline-none bg-white border border-gray-300 rounded-full transition-all duration-300 focus:ring-2 focus:ring-indigo-300"
             />
             <FaSearch
               className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
               size={20}
             />
           </div>
-
-          <a href="#" className="hover:text-gray-500">
-            <FaShoppingCart size={20} />
-          </a>
-          <a href="#" className="hover:text-gray-500">
-            <FaHeart size={20} />
-          </a>
-          <Link
-            to="/auth"
-            className="bg-white cursor-pointer text-black font-semibold hover:bg-black hover:text-white duration-300 border-2 border-gray-300 tracking-widest py-2 px-4"
-          >
-            Sign In
+          <Link to="/" className="hover:text-gray-500 flex items-center">
+            <AiOutlineShoppingCart size={20} className="ss:mr-1" />{" "}
+            <span className="ss:block hidden">Cart</span>
           </Link>
-          <Link
-            to="/register"
-            className="bg-black cursor-pointer text-white font-semibold py-2 px-4 hover:bg-white hover:text-black duration-300 border-2 border-black tracking-widest"
-          >
-            Sign Up
+          <Link to="/" className="hover:text-gray-500 flex items-center">
+            <MdFavoriteBorder size={20} className="ss:mr-1" />{" "}
+            <span className="ss:block hidden">Favorites</span>
+          </Link>
+          <Link to="/auth" className="flex hover:text-gray-500 items-center">
+            <GoPerson size={20} className="ss:mr-2" />
+            <span className="ss:block hidden">Your account</span>
           </Link>
         </div>
       </div>
+      <div className="relative max-w-md mx-auto ss:hidden px-4 mb-2">
+        <input
+          type="text"
+          placeholder="Search products..."
+          className="py-2 px-3 pr-10 text-gray-700 w-full focus:outline-none bg-white border border-gray-300 rounded-full transition-all duration-300 focus:ring-2 focus:ring-indigo-300"
+        />
+        <FaSearch
+          className="absolute right-7 top-1/2 transform -translate-y-1/2 text-gray-400"
+          size={20}
+        />
+      </div>
       <hr className=" border-gray-700" />
-      <div className="bg-black">
-        <ul className="flex justify-around w-1/2 mx-auto text-white text-center">
+      <div className="bg-black xs:block hidden">
+        <ul className="flex justify-around  mx-auto text-white text-center md:max-w-[1000px] xs:text-sm sm:text-base">
           <li className="bg-black cursor-pointer text-white font-semibold py-2 px-4 hover:bg-white hover:text-black duration-300 tracking-widest w-1/5">
             Home
           </li>
@@ -49,13 +56,13 @@ const Navbar = () => {
             Shop
           </li>
           <li className="bg-black cursor-pointer text-white font-semibold py-2 px-4 hover:bg-white hover:text-black duration-300 tracking-widest w-1/5">
+            Best Sales
+          </li>
+          <li className="bg-black cursor-pointer text-white font-semibold py-2 px-4 hover:bg-white hover:text-black duration-300 tracking-widest w-1/5">
             About
           </li>
           <li className="bg-black cursor-pointer text-white font-semibold py-2 px-4 hover:bg-white hover:text-black duration-300 tracking-widest w-1/5">
-            Contact Us
-          </li>
-          <li className="bg-black cursor-pointer text-white font-semibold py-2 px-4 hover:bg-white hover:text-black duration-300 tracking-widest w-1/5">
-            Best Sales
+            Contact
           </li>
         </ul>
       </div>
