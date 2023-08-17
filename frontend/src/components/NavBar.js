93502526d2967bbe96cfa045c5dc465c31567b8b
Zouaoui Mohamed Cherif <cheriffzouaoui@gmail.com>
2023-08-17 15:20:41 +0100

    Navbar styling

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,17 +4,21 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="bg-stone-200 text-black w-full">
-      <div className="container mx-auto flex justify-between items-center py-4">
+      <div className="container mx-auto flex justify-between items-center py-3">
         <div className="text-xl font-bold">LOGO</div>
         <div className="flex items-center space-x-6">
-          <div className="flex items-center bg-white">
+          <div className="relative">
             <input
               type="text"
-              placeholder="Search"
-              className="p-2 text-black w-40 sm:w-56 focus:outline-none"
+              placeholder="Search products..."
+              className="py-3 px-4 pr-10 text-gray-700 w-64 sm:w-96 focus:outline-none bg-white border border-gray-300 rounded-full transition-all duration-300 focus:ring-2 focus:ring-indigo-300"
             />
-            <FaSearch className=" text-gray-400 mr-2" size={20} />
+            <FaSearch
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+              size={20}
+            />
           </div>
+
           <a href="#" className="hover:text-gray-500">
             <FaShoppingCart size={20} />
           </a>
@@ -23,19 +27,38 @@ const Navbar = () => {
           </a>
           <Link
             to="/auth"
-            className="bg-white cursor-pointer text-black font-semibold hover:bg-black hover:text-white duration-700 border-2 border-white tracking-widest py-2 px-4"
+            className="bg-white cursor-pointer text-black font-semibold hover:bg-black hover:text-white duration-300 border-2 border-gray-300 tracking-widest py-2 px-4"
           >
             Sign In
           </Link>
           <Link
             to="/register"
-            className="bg-black cursor-pointer text-white font-semibold py-2 px-4 hover:bg-white hover:text-black duration-700 border-2 border-black tracking-widest"
+            className="bg-black cursor-pointer text-white font-semibold py-2 px-4 hover:bg-white hover:text-black duration-300 border-2 border-black tracking-widest"
           >
             Sign Up
           </Link>
         </div>
       </div>
       <hr className=" border-gray-700" />
+      <div className="bg-black">
+        <ul className="flex justify-around w-1/2 mx-auto text-white text-center">
+          <li className="bg-black cursor-pointer text-white font-semibold py-2 px-4 hover:bg-white hover:text-black duration-300 tracking-widest w-1/5">
+            Home
+          </li>
+          <li className="bg-black cursor-pointer text-white font-semibold py-2 px-4 hover:bg-white hover:text-black duration-300 tracking-widest w-1/5">
+            Shop
+          </li>
+          <li className="bg-black cursor-pointer text-white font-semibold py-2 px-4 hover:bg-white hover:text-black duration-300 tracking-widest w-1/5">
+            About
+          </li>
+          <li className="bg-black cursor-pointer text-white font-semibold py-2 px-4 hover:bg-white hover:text-black duration-300 tracking-widest w-1/5">
+            Contact Us
+          </li>
+          <li className="bg-black cursor-pointer text-white font-semibold py-2 px-4 hover:bg-white hover:text-black duration-300 tracking-widest w-1/5">
+            Best Sales
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
